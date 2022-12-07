289d376af32d8e3c0640596f8fbe991837cb4f2f
jin7942 <infomer.jin@gmail.com>
2022-12-07 14:47:07 +0900

    yea'

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,6 +5,9 @@ const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
 const call = document.getElementById('call');
+const chatInput = document.getElementById('chatInput');
+const sendMsg = document.getElementById('sendMsg');
+const chatList = document.getElementById('chatList');
 
 call.hidden = true;
 
@@ -12,6 +15,8 @@ let mystream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
     try {
@@ -81,6 +86,11 @@ cameraBtn.addEventListener('click', () => {
 });
 camerasSelect.addEventListener('change', async () => {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === 'video');
+        videoSender.replaceTrack(videoTrack);
+    }
 });
 
 // Welcome form
@@ -88,22 +98,100 @@ camerasSelect.addEventListener('change', async () => {
 const welcome = document.getElementById('welcome');
 const welcomeForm = welcome.querySelector('form');
 
-const startMedia = () => {
+const initCall = async () => {
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 };
 
-welcomeForm.addEventListener('submit', (e) => {
+welcomeForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const input = welcomeForm.querySelector('input');
-    socket.emit('join_room', input.value, startMedia);
+    await initCall();
+    socket.emit('join_room', input.value);
     roomName = input.value;
     input.value = '';
 });
 
-// Socket Code
+const createMsg = () => {
+    chatInput.addEventListener('keyup', (e) => {
+        if (e.keyCode == 13) {
+            sendMsg.click();
+        }
+    });
+    sendMsg.addEventListener('click', () => {
+        createChatList(chatInput.value);
+        myDataChannel.send(chatInput.value);
+        chatInput.value = '';
+    });
+};
 
-socket.on('welcome', () => {
-    console.log('someone joined');
+const createChatList = (msg) => {
+    const li = document.createElement('li');
+    li.innerText = '익명의 누군가: ' + msg;
+    chatList.appendChild(li);
+};
+
+// Socket Code
+socket.on('welcome', async () => {
+    myDataChannel = myPeerConnection.createDataChannel('chat');
+    console.log('create data channel');
+    myDataChannel.addEventListener('message', (e) => {
+        createChatList(e.data);
+    });
+
+    createMsg();
+
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit('offer', offer, roomName);
 });
+
+// offer
+socket.on('offer', async (offer) => {
+    myPeerConnection.addEventListener('datachannel', (e) => {
+        myDataChannel = e.channel;
+        myDataChannel.addEventListener('message', (e) => {
+            createChatList(e.data);
+        });
+    });
+
+    createMsg();
+
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit('answer', answer, roomName);
+});
+// answer
+socket.on('answer', (answer) => {
+    console.log('receive the offer');
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+    console.log('recive candidate');
+    myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+
+const makeConnection = () => {
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302', 'stun:stun3.l.google.com:19302'],
+            },
+        ],
+    });
+    myPeerConnection.addEventListener('icecandidate', (data) => {
+        console.log('sent candidate');
+        socket.emit('ice', data.candidate, roomName);
+    });
+    myPeerConnection.addEventListener('addstream', (data) => {
+        const peerFace = document.getElementById('peerFace');
+        peerFace.srcObject = data.stream;
+    });
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+};
--- src/server.js
+++ src/server.js
@@ -19,12 +19,21 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIo(httpServer);
 
 wsServer.on('connection', (socket) => {
-    socket.on('join_room', (roomName, done) => {
-        console.log(roomName);
+    socket.on('join_room', (roomName) => {
         socket.join(roomName);
-        done();
-
         socket.to(roomName).emit('welcome');
+    });
+
+    socket.on('offer', (offer, roomName) => {
+        socket.to(roomName).emit('offer', offer);
+    });
+
+    socket.on('answer', (answer, roomName) => {
+        socket.to(roomName).emit('answer', answer);
+    });
+
+    socket.on('ice', (ice, roomName) => {
+        socket.to(roomName).emit('ice', ice);
     });
 });
 
